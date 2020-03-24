d686eccbefc7b4423485094fe2e16ffdb41e2227
vincent930093 <vincent930093@gmail.com>
2020-03-24 09:11:28 +0800

    weapon.h裡的東西原本放錯地方

--- game/Source/Weapon.cpp
+++ game/Source/Weapon.cpp
@@ -15,30 +15,5 @@ namespace game_framework {
 	/////////////////////////////////////////////////////////////////////////////
 	// CBall: Ball class
 	/////////////////////////////////////////////////////////////////////////////
-	class Weapon
-	{
-	public:
-		Weapon();
-		~Weapon();
-		void LoadBitap();
-		void ShowBitmap();
-		void setXY(int x, int y);
-		void skillQ();
-		void skillW();
-		void skillE();
-		void skillR();
-		void NAttack();
-
-	private:
-		int _x, _y;
-	};
-
-	Weapon::Weapon()
-	{
-	}
-
-	Weapon::~Weapon()
-	{
-	}
 	
 }
